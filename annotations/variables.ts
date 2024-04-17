9873026b3e60c10e8123413a5cc9941567b2a131
Chanda Sukesh <chandasukesh@gmail.com>
2024-04-18 00:39:47 +0530

    type annotations examples

---
+++ annotations/variables.ts
@@ -0,0 +1,24 @@
+// type annotation example
+
+const apple : number=5;
+
+// throws error
+// const apple1 : number=false;
+
+let apple2 :number=5;
+// throws error
+// apple2='hello'
+
+let speed:string='speed'
+// throws error
+// let speed:string=10
+
+let nothingMuch: null= null;
+
+let nothingMuchUndefined: undefined= undefined;
+
+
+// examples of built in objects
+ let now : Date = new Date()
+
+
