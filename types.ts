9f50fa8ca41a96c20bf275041949363a04d478e0
Chanda Sukesh <chandasukesh@gmail.com>
2024-04-18 00:19:55 +0530

    types feauture example

---
+++ types.ts
@@ -0,0 +1,23 @@
+const date= new Date()
+
+// as date know the Date type, it gives properties and methods as auto suggestions
+date.getDay()
+
+// throws erro
+// date.asfff
+
+const objectAge={
+    age:10
+}
+
+objectAge.age
+
+// throws error
+// objectAge.gfjfjfj
+
+class Color{}
+
+const red=new Color()
+
+// no auto suggestions as we didnt mentioned anything in color class
+// red.
