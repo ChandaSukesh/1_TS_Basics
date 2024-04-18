86c431f32664bd993ecb80afb555538690e0ade7
Chanda Sukesh <chandasukesh@gmail.com>
2024-04-19 00:21:07 +0530

    anonymns function example

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -3,3 +3,15 @@ const addSum = (a:number,b:number) : number=>{
 }
 const sum=addSum(1,2)
 console.log("suk1",sum);
+
+
+// function declaration by function keyword
+
+function divide(a:number,b:number):number{
+    return a/b;
+}
+
+// anonyms function 
+const multiply = function(a:number,b:number):number {
+    return a*b;
+}
