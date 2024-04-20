0392b2d671b47693039983c7c21c981909069506
Chanda Sukesh <chandasukesh@gmail.com>
2024-04-20 14:32:10 +0530

    multiple types array example

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,7 +1,7 @@
 const myCars : string[]=['Ford','BMW','Audi']
 
 
-// why do we care point 1 ex:
+// why do we care point 1 ex:, help with inference while extracting values
 const singleCar=myCars[0]
 const lastCar=myCars.pop()
 
@@ -14,4 +14,9 @@ const upperCaseCars= myCars.map((car:string):string=>{
     return car.toUpperCase()
 })
 
-// point 4 example
+// point 4 example flexible types
+const importantDates: (Date | string)[]=[new Date()]
+importantDates.push('20-30-1997');
+importantDates.push(new Date())
+// error
+// importantDates.push(100)
